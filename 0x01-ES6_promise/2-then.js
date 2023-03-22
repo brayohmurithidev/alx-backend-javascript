ca62f2c06cd049c3242ed39c83e4cc6ecb3f0715
brayohmurithidev <murithibrianm@gmail.com>
2023-03-22 23:45:56 +0300

    catch error

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,11 @@
-export default function handleResponseFromAPI(promise) {
-  const mypromise = new Promise((resolve, reject) => {
-    if (promise) {
-      resolve({
-        status: 200,
-        body: 'success',
-      });
-    } else {
-      reject(Error);
-    }
-  });
-
-  return mypromise.then((console.error('Got a response from the API')));
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => Error())
+    .finally(() => console.log('Got a response from the API'));
 }
+
+export default handleResponseFromAPI;
